08e09b5e60883600f361a381114a698f8059bcf8
ndurushima <ndu1994@gmail.com>
2025-08-04 19:42:24 -0500

    Create instance methods bday celeb, and checkup.

--- lib/dog.py
+++ lib/dog.py
@@ -4,3 +4,11 @@ class Dog:
         self.breed = breed
         self.age = age
         self.last_checkup = last_checkup
+    
+    def checkup(self, date):
+        print(f"Checking up with {self.name} on {date}.")
+        self.last_checkup = date
+    
+    def birthday_celebration(self):
+        self.age += 1
+        print(f"{self.name} is turning {self.age}")
