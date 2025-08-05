af1eb21dd3b561f9be2fc93b8eaa0c56a8981c3f
ndurushima <ndu1994@gmail.com>
2025-08-04 19:38:23 -0500

    Initialize Instance Properties. Also init last check-up. if the user puts nothing in for last_checkup then it will default to None.

--- lib/dog.py
+++ lib/dog.py
@@ -1,2 +1,6 @@
 class Dog:
-    pass
+    def __init__(self, name, breed, age, last_checkup = None):
+        self.name = name
+        self.breed = breed
+        self.age = age
+        self.last_checkup = last_checkup
