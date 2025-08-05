0a0714ea57142274981ac9039435badd539e2f5a
ndurushima <ndu1994@gmail.com>
2025-08-04 19:51:18 -0500

    Create and use instances of the class.

--- lib/dog.py
+++ lib/dog.py
@@ -12,3 +12,19 @@ class Dog:
     def birthday_celebration(self):
         self.age += 1
         print(f"{self.name} is turning {self.age}")
+
+fido = Dog("Fido", "Golden Retriever", 3, "05/22/2022")
+
+clifford = Dog(
+    name = "Clifford",
+    breed = "Big Red",
+    age = 2
+)
+
+print(fido.age)
+fido.birthday_celebration()
+print(fido.age)
+
+print(clifford.last_checkup)
+clifford.checkup("03/02/2024")
+print(clifford.last_checkup)
